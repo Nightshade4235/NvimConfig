c9c4be04952030dbf935f60b964dbbeb8686dd0c
Nightshade <jademountainacademy0@gmail.com>
2025-10-04 19:17:49 +0530

    Update lsp-config.lua
    bug fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,21 +16,13 @@ return {
   { 
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.asm_lsp.setup({})
-      lspconfig.omnisharp.setup({})
-      lspconfig.html.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.jdtls.setup({})
-      lspconfig.pyright.setup({})
-
+      local lspconfig = require("lspconfig") -- in line after this for every lsp you have entered in line 12 right an inclusion for it below in the format:- ``` lspconfig.<yourlsp>.setup({})  ```  replace <yourlsp> with one of your lsps. Do the same for all of your lsps.
+      --enter the lspconfig.<yourlsp>.setup({}) here
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
+
 
